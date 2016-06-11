a7b004253b558f86964e11a35bd114325760027f
zixie1991 <zixie1991@163.com>
2016-06-11 22:51:45 +0800

    update kmp

--- leetcode/string/kmp.cc
+++ leetcode/string/kmp.cc
@@ -9,7 +9,17 @@ using namespace std;
 // KMP 算法
 // 字符串匹配之 KMP 和 BM 算法: http://www.oschina.net/question/12_23429?fromerr=ARH1VIkN
 
-char* Strstr(const char* text, const char* pattern) {
+/**
+ * @brief 暴力前缀匹配
+ *
+ * 时间复杂度 O(n*m)
+ *
+ * @param text 文本串
+ * @param pattern 模式串
+ *
+ * @return 
+ */
+char* StrStr(const char* text, const char* pattern) {
   int pattern_len = strlen(pattern); 
   if (pattern_len == 0) {
     return (char*)text;
@@ -27,19 +37,19 @@ char* Strstr(const char* text, const char* pattern) {
 /**
  * @brief 部分匹配表
  *
- * @param str 搜索词(模式串)
- * @param n 搜索词长度
+ * @param pattern 模式串
+ * @param pattern_len 模式串长度
  * @param next 部分匹配表
  */
-void PartialMatch(const char* str, int n, int* next) {
+void PartialMatch(const char* pattern, int pattern_len, int* next) {
   int j = 0; // j表示前缀
   next[0] = next[1] = j;
-  for (int i = 2; i < n; i++) { // i表示后缀
-    while (j > 0 && str[j] != str[i-1]) {
+  for (int i = 2; i < pattern_len; i++) { // i表示后缀
+    while (j > 0 && pattern[j] != pattern[i-1]) {
       j = next[j];
     }
 
-    if (str[j] == str[i-1]) {
+    if (pattern[j] == pattern[i-1]) {
       j++;
     }
 
@@ -50,44 +60,43 @@ void PartialMatch(const char* str, int n, int* next) {
 /**
  * @brief KMP算法
  *
- * @param str1 字符串(文本串)
- * @param n 字符串长度
- * @param str2 搜索词(模式串)
- * @param m 搜索词长度
+ * @param text 字符串(文本串)
+ * @param pattern 搜索词(模式串)
  *
  * @return 
  */
-int KMP(const char* str1, int n, const char* str2, int m) {
+char* KMP(const char* text, const char* pattern) {
+  int text_len = strlen(text);
+  int pattern_len = strlen(pattern);
   // 部分匹配表
-  // next[i]表示str2[0..i-1]的"前缀"和"后缀"的最长公共元素长度
-  int* next = (int*)malloc(m * sizeof(int));
-  PartialMatch(str2, m, next);
-  for (int i = 0; i < m; i++) {
-    printf("%d ", next[i]);
-  }
-  printf("\n");
+  // next[i]表示pattern[0..i-1]的"前缀"和"后缀"的最长公共元素长度
+  int* next = (int*)malloc(pattern_len * sizeof(int));
+  PartialMatch(pattern, pattern_len, next);
 
   int j = 0;
-  for (int i = 0; i < n; i++) {
-    while (j > 0 && str1[i] != str2[j]) {
+  for (int i = 0; i < text_len; i++) {
+    while (j > 0 && text[i] != pattern[j]) {
       j = next[j];
     }
-    if (str1[i] == str2[j]) {
+    if (text[i] == pattern[j]) {
       j++;
     }
-    if (j == m) {
+    if (j == pattern_len) {
       free(next);
-      return i - m + 1; 
+      return (char*)text + i - pattern_len + 1; 
     }
   }
 
   free(next);
-  return -1;
+  return NULL;
 }
 
 int main() {
-  const char* str1 = "ababacacca";
-  const char* str2 = "b";
-  cout << KMP(str1, strlen(str1), str2, strlen(str2)) << endl;
+  const char* text = "ababacacca";
+  const char* pattern = "b";
+  cout << "text: " << text << endl;
+  cout << "pattern: " << pattern << endl;
+  cout << "kmp: " << KMP(text, pattern) << endl;
+  cout << "strstr: " << StrStr(text, pattern) << endl;
   return 0;
 }
