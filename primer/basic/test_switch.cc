56a3bac020a13337f69f033e79e40b9c44c1aa0b
zixie1991 <zixie1991@163.com>
2016-06-19 10:05:34 +0800

    add switch

---
+++ primer/basic/test_switch.cc
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << (int)'a' << " " << (int)'A' << " " << (int)'0' << endl;
+  return 0;
+}
