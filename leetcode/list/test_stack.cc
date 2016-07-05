71c13d7a372cdb207f7023cd7334083e42b33b3b
zixie1991 <zixie1991@163.com>
2016-07-05 19:34:35 +0800

    add QStack: two queue release stack

--- leetcode/list/test_stack.cc
+++ leetcode/list/test_stack.cc
@@ -4,6 +4,7 @@
 #include <assert.h>
 
 #include <iostream>
+#include <queue>
 
 using namespace std;
 
@@ -51,6 +52,96 @@ void StackPop(Stack* stack) {
   stack->length--;
 }
 
+template <typename T>
+class QStack {
+  public:
+    void Push(T val) {
+      // 选择q1, q2中非空的入队
+      if (q1_.empty()) {
+        // q1空
+        q2_.push(val);
+      } else {
+        // q1非空, q2空
+        q1_.push(val);
+      }
+    }
+
+    void Pop() {
+      if (q1_.empty() && q2_.empty()) {
+        return ;
+      }
+
+      // q1, q2一个空，一个非空
+      if (q1_.empty()) {
+        // q2非空
+        while (q2_.size() > 1) {
+          T v = q2_.front();
+          q2_.pop();
+          q1_.push(v);
+        }
+
+        // 出栈
+        q2_.pop();
+      } else {
+        // q1非空
+        while (q1_.size() > 1) {
+          T v = q1_.front();
+          q1_.pop();
+          q2_.push(v);
+        }
+        
+        // 出栈
+        q1_.pop();
+      }
+    }
+
+    T Top() {
+      assert(!q1_.empty() || !q2_.empty());
+      T ret;
+
+      // q1, q2一个空，一个非空
+      if (q1_.empty()) {
+        // q2非空
+        while (q2_.size() > 1) {
+          T v = q2_.front();
+          q2_.pop();
+          q1_.push(v);
+        }
+
+        // 栈顶
+        ret = q2_.front();
+        q2_.pop();
+        q1_.push(ret);
+      } else {
+        // q1非空
+        while (q1_.size() > 1) {
+          T v = q1_.front();
+          q1_.pop();
+          q2_.push(v);
+        }
+        
+        // 栈顶
+        ret = q1_.front();
+        q1_.pop();
+        q2_.push(ret);
+      }
+
+      return ret;
+    }
+
+    int Size() {
+      return q1_.size() + q2_.size();
+    }
+
+    bool Empty() {
+      return q1_.empty() && q2_.empty();
+    }
+
+  private:
+    queue<T> q1_;
+    queue<T> q2_;
+};
+
 void TestStackPushAndPop() {
   Stack s;
   StackPush(&s, 1);
@@ -75,8 +166,34 @@ void TestStackPushAndPop() {
   cout << StackLength(&s) << endl;
 }
 
+void TestQStackPushAndPop() {
+  QStack<int> s;
+  s.Push(1);
+  s.Push(2);
+  s.Push(3);
+  s.Push(4);
+
+  cout << s.Top() << endl;
+  s.Pop();
+  cout << s.Top() << endl;
+  s.Pop();
+  cout << s.Top() << endl;
+  s.Pop();
+  s.Push(5);
+  cout << s.Top() << endl;
+  s.Pop();
+
+  s.Push(6);
+  cout << s.Top() << endl;
+  s.Pop();
+
+  cout << s.Size() << endl;
+}
+
 int main() {
   cout << "TestStackPushAndPop:" << endl;
   TestStackPushAndPop();
+  cout << "TestQStackPushAndPop:" << endl;
+  TestQStackPushAndPop();
   return 0;
 }
