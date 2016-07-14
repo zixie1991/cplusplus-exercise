e8fc4ec761921213ffc5146415ee938cd1540a5e
zixie1991 <zixie1991@163.com>
2016-07-14 10:12:58 +0800

    add atoi

---
+++ leetcode/string/atoi.cc
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <iostream>
+
+using namespace std;
+
+int Atoi(const char* str) {
+  // 错误处理
+  // 检查输入(NULL, "")
+  // 符号
+  // 非法字符
+  // 越界
+}
+
+int main() {
+  return 0;
+}
