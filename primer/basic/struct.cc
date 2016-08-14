a57fae341eabd45be4b97382dcf2a3f7ab2c615d
zixie1991 <zixie1991@163.com>
2016-08-14 19:20:59 +0800

    update basic

---
+++ primer/basic/struct.cc
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include <iostream>
+
+using namespace std;
+
+// sizeof(struct type)等于所有成员的内存长度的和
+
+int main() {
+  return 0;
+}
--- primer/basic/test_switch.cc
+++ primer/basic/test_switch.cc
@@ -6,6 +6,26 @@
 
 using namespace std;
 
+enum Mode{kNew=-1, kRead, kWrite};
+
+void TestSwitchCaseType() {
+  // type require integer
+  float f;
+  switch (static_cast<int>(f)) {
+    case int(1.0):
+    case int(2.0):
+      break;
+  }
+
+  enum Mode mode;
+  switch (mode) {
+    case kNew:
+    case kRead:
+    case kWrite:
+      break;
+  }
+}
+
 int main() {
   cout << (int)'a' << " " << (int)'A' << " " << (int)'0' << endl;
   return 0;
